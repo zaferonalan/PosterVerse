5af281805ed124263437b357a309346da7d61314
Zafer Günay Önalan <zafergunay7@gmail.com>
2026-07-05 17:14:40 +0300

    chore: database prisma eklendi

---
+++ my-app/packages/database/src/index.ts
@@ -0,0 +1,4 @@
+export { prisma } from "./client"; // exports instance of prisma
+export * from "./generated/prisma/client/client"; // exports generated types from prisma
+export { PrismaService } from './prisma.service';
+export { PrismaModule } from './prisma.module';
